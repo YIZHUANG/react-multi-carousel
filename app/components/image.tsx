0527ddfcfd5fd3783663fa927f868b489a2235d3
YIZHUANG <yi.zhuang1@outlook.com>
2019-04-08 20:55:38 +0300

    Added visibile and active props to carousel items

--- app/components/image.tsx
+++ app/components/image.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
 
-
-const Image = ({ url, alt }) => (
-  <img draggable={false} style={{ width: '100%', height: '100%',position: 'relative'}} src={url} alt={alt} />
-)
+const Image = ({ url, alt, ...rest }) => {
+  return (
+    <img
+      draggable={false}
+      style={{ width: "100%", height: "100%", position: "relative" }}
+      src={url}
+      alt={alt}
+    />
+  );
+};
 
 export default Image;
