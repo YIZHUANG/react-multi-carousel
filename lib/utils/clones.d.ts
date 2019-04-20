86ea2d3fee29636d7ee8f27f1b4c734e115fb47c
YIZHUANG <yi.zhuang1@outlook.com>
2019-04-20 15:57:54 +0300

    New build for pre-swtiching the clones position for the inifinite mode for smoother animation in very rare cases

--- lib/utils/clones.d.ts
+++ lib/utils/clones.d.ts
@@ -13,10 +13,10 @@ declare function getClones(slidesToShow: number, childrenArr: any[]): {
     clones: any[];
     initialSlide: number;
 };
-declare function whenEnteredClones({ currentSlide, slidesToShow, itemWidth, totalItems }: CarouselInternalState, childrenArr: any[], props: CarouselProps): {
-    hasEnterClonedAfter: boolean;
-    hasEnterClonedBefore: boolean;
+declare function checkClonesPosition({ currentSlide, slidesToShow, itemWidth, totalItems }: CarouselInternalState, childrenArr: any[], props: CarouselProps): {
+    isReachingTheEnd: boolean;
+    isReachingTheStart: boolean;
     nextSlide: number;
     nextPosition: number;
 };
-export { getOriginalCounterPart, getCloneCounterPart, getClones, whenEnteredClones };
+export { getOriginalCounterPart, getCloneCounterPart, getClones, checkClonesPosition };
--- lib/utils/index.d.ts
+++ lib/utils/index.d.ts
@@ -1,4 +1,4 @@
-import { getOriginalCounterPart, getCloneCounterPart, getClones, whenEnteredClones } from "./clones";
+import { getOriginalCounterPart, getCloneCounterPart, getClones, checkClonesPosition } from "./clones";
 import { getWidthFromDeviceType, getParitialVisibilityGutter, getItemClientSideWidth } from "./elementWidth";
 import { getInitialState, getIfSlideIsVisbile, getTransformForCenterMode, getTransformForPartialVsibile } from './common';
 import throttle from "./throttle";
@@ -6,4 +6,4 @@ import throwError from './throwError';
 import { populateNextSlides } from './next';
 import { populatePreviousSlides } from './previous';
 import { populateSlidesOnMouseTouchMove } from './mouseOrTouchMove';
-export { getOriginalCounterPart, getCloneCounterPart, getClones, getWidthFromDeviceType, whenEnteredClones, getItemClientSideWidth, getParitialVisibilityGutter, throttle, getInitialState, getIfSlideIsVisbile, getTransformForCenterMode, getTransformForPartialVsibile, throwError, populateNextSlides, populatePreviousSlides, populateSlidesOnMouseTouchMove };
+export { getOriginalCounterPart, getCloneCounterPart, getClones, getWidthFromDeviceType, checkClonesPosition, getItemClientSideWidth, getParitialVisibilityGutter, throttle, getInitialState, getIfSlideIsVisbile, getTransformForCenterMode, getTransformForPartialVsibile, throwError, populateNextSlides, populatePreviousSlides, populateSlidesOnMouseTouchMove };
