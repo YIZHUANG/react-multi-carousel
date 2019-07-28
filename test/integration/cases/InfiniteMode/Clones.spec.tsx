37370289cf5ece853ae31c2f6b270ced3d81c012
YIZHUANG <yi.zhuang1@outlook.com>
2019-07-28 16:57:25 +0300

    Fixed test

--- test/integration/cases/InfiniteMode/Clones.spec.tsx
+++ test/integration/cases/InfiniteMode/Clones.spec.tsx
@@ -1,16 +1,13 @@
 import * as React from "react";
 import { configure, shallow, mount } from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
-import toJson from "enzyme-to-json";
 
 configure({ adapter: new Adapter() });
 
 import Carousel from "../../../../lib/Carousel";
-import { LeftArrow, RightArrow } from "../../../../lib/Arrows";
-import { CustomLeftArrow, CustomRightArrow } from "../../components/Arrows";
 import Card from "../../components/Card";
-import { responsive1 } from '../../common/responsive';
-import { longData, shortData } from '../../common/data';
+import { responsive1 } from "../../common/responsive";
+import { longData, shortData } from "../../common/data";
 
 /*
 The current setting is if the length of the carousel item is larger than "slidesToShow * 2",
@@ -20,33 +17,24 @@ Otherwise, it means we only have a few items. Then we clone it 3 times.
 describe("Clones", () => {
   test("Gets clones for small amount of data 3 items the length", () => {
     const wrapper = mount(
-      <Carousel
-        responsive={responsive1}
-        infinite={true}
-        minimumTouchDrag={0}
-      >
-      {shortData.map(card => {
-        return <Card {...card} />;
-      })}
+      <Carousel responsive={responsive1} infinite={true} minimumTouchDrag={0}>
+        {shortData.map(card => {
+          return <Card {...card} />;
+        })}
       </Carousel>
     );
-    console.log(wrapper.state().clones.length);
-    expect(wrapper.state().clones.length).toBe(shortData.length * 3);
+    expect(wrapper.state().totalItems).toBe(shortData.length * 3);
     wrapper.unmount();
   });
   test("Gets clones for large amount of data for beginning and end set of items", () => {
     const wrapper = mount(
-      <Carousel
-        responsive={responsive1}
-        infinite={true}
-        minimumTouchDrag={0}
-      >
-      {longData.map(card => {
-        return <Card {...card} />;
-      })}
+      <Carousel responsive={responsive1} infinite={true} minimumTouchDrag={0}>
+        {longData.map(card => {
+          return <Card {...card} />;
+        })}
       </Carousel>
     );
-    expect(wrapper.state().clones.length).toBe(24);
+    expect(wrapper.state().totalItems).toBe(24);
     wrapper.unmount();
   });
 });
--- test/unit/clones.spec.ts
+++ test/unit/clones.spec.ts
@@ -1,33 +1,30 @@
-import { getClones } from "./utils";
+import { getClones, getInitialSlideInInifteMode } from "./utils";
 
 describe("Infinite mode for clones", () => {
   describe("getClones functionality", () => {
     test("Less than slidesToShow * 2", () => {
       const slidesToShow = 2;
       const children = [1, 2, 3];
-      const results = getClones(slidesToShow, children);
-      expect(results).toEqual({
-        clones: [1, 2, 3, 1, 2, 3, 1, 2, 3],
-        initialSlide: 3
-      });
+      const initialSlide = getInitialSlideInInifteMode(slidesToShow, children);
+      const clones = getClones(slidesToShow, children);
+      expect(clones).toEqual([1, 2, 3, 1, 2, 3, 1, 2, 3]);
+      expect(initialSlide).toEqual(3);
     });
     test("Large than slidesToShow * 2", () => {
       const slidesToShow = 2;
       const children = [1, 2, 3, 1, 2, 3];
-      const results = getClones(slidesToShow, children);
-      expect(results).toEqual({
-        clones: [3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1],
-        initialSlide: 4
-      });
+      const initialSlide = getInitialSlideInInifteMode(slidesToShow, children);
+      const clones = getClones(slidesToShow, children);
+      expect(clones).toEqual([3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1]);
+      expect(initialSlide).toEqual(4);
     });
     test("The same amount of slidesToShow * 2", () => {
       const slidesToShow = 2;
       const children = [1, 2, 3, 1];
-      const results = getClones(slidesToShow, children);
-      expect(results).toEqual({
-        clones: [1, 2, 3, 1, 1, 2, 3, 1, 1, 2, 3, 1],
-        initialSlide: 4
-      });
+      const initialSlide = getInitialSlideInInifteMode(slidesToShow, children);
+      const clones = getClones(slidesToShow, children);
+      expect(clones).toEqual([1, 2, 3, 1, 1, 2, 3, 1, 1, 2, 3, 1]);
+      expect(initialSlide).toEqual(4);
     });
   });
 });
