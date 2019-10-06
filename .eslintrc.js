0f7c34c435180d95eda0873087f2c651d15b3a12
raajnadar <raajnadar@gmail.com>
2019-10-06 17:10:55 +0530

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions:  {
+  ecmaVersion:  2018,
+  sourceType:  'module',
+  ecmaFeatures:  {
+    jsx:  true,
+  },
+  },
+  rules:  {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+  },
+  settings:  {
+    react:  {
+      version:  'detect',
+    },
+  },
+};
